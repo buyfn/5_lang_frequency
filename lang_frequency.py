4e319321f379f4d236b3d51dbf0a94a0f3680185
ignat <buyfng@gmail.com>
2017-06-23 15:04:39 +0300

    use collections.Counter, separate function for printing

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,37 +2,33 @@ import os
 import sys
 import string
 import operator
+import collections
 
 
-def load_data(filepath):
+def load_text(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, 'r') as file_handler:
         return file_handler.read()
 
 
-def count_words(word_list):
-    word_count = {}
-    for word in word_list:
-        if word in word_count.keys():
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
-    return word_count
-
-
 def get_most_frequent_words(text, count=10):
     punctuation = string.punctuation + '—«»…'
-    stripped_text = ''.join([char.lower() for char in file_content
+    stripped_text = ''.join([char.lower() for char in text
                              if char not in punctuation])
     words = stripped_text.split()
-    word_count = count_words(words)
+    word_count = collections.Counter(words)
 
     sorted_words = sorted(word_count.items(),
                           key=operator.itemgetter(1),
                           reverse=True)
 
     return sorted_words[:count]
+
+
+def print_frequent_words(words_dict):
+    for word in words_dict:
+        print('{}: {}'.format(word[0], word[1]))
 
 
 if __name__ == '__main__':
@@ -42,11 +38,10 @@ if __name__ == '__main__':
     except IndexError:
         sys.exit('Need to specify text file to count words in')
 
-    file_content = load_data(path)
-    if file_content:
-        frequent_words = get_most_frequent_words(file_content)
+    text = load_text(path)
+    if text:
+        frequent_words = get_most_frequent_words(text, 100)
         print('\nMost frequent words in the file:')
-        for elt in frequent_words:
-            print('{}: {}'.format(elt[0], elt[1]))
+        print_frequent_words(frequent_words)
     else:
         print('Wrong filepath')
